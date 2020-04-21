0323037aee7261950fcb854f4f5c7af813409687
ClessLi <316600949@qq.com>
2020-04-21 11:50:11 +0800

    修正备份命令指定工作目录的方法

--- pkg/resolv/backup.go
+++ pkg/resolv/backup.go
@@ -61,7 +61,7 @@ func tgz(path string, list []string) (err error) {
 	args := append([]string{"-zcf", path}, list...)
 	cmd := exec.Command(tar, args...)
 	cmd.Stderr = os.Stderr
-	cmd.Path = filepath.Dir(path)
+	cmd.Dir = filepath.Dir(path)
 	err = cmd.Run()
 	return
 }
