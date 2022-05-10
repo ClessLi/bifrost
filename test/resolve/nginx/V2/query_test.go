9c470a60a2bf600a3a3e89d09b64f2f898003c55
ClessLi <316600949@qq.com>
2022-05-10 17:01:51 +0800

    test(resolv): non-cascaded query test

---
+++ test/resolve/nginx/V2/query_test.go
@@ -0,0 +1,37 @@
+package V2_test
+
+import (
+	"github.com/ClessLi/bifrost/pkg/resolv/V2/nginx/configuration"
+	"github.com/ClessLi/bifrost/pkg/resolv/V2/nginx/configuration/parser"
+	"github.com/ClessLi/bifrost/pkg/resolv/V2/nginx/parser_type"
+	"strings"
+	"testing"
+)
+
+func TestQueryNonCascaded(t *testing.T) {
+
+	configPath := "../../../nginx/conf/nginx.conf"
+	config, err := configuration.NewConfigurationFromPath(configPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testLocation, err := config.Query(`location:sep: :reg: /test1-location`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	proxyPassKW, err := parser.NewKeyWords(parser_type.TypeKey, true, `^proxy_pass\s+`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	proxyPassKW.SetCascaded(false)
+	ctx, idx := testLocation.Self().(*parser.Location).Query(proxyPassKW)
+	childP, err := ctx.GetChild(idx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.EqualFold(childP.GetValue(), "proxy_pass http://right_proxy") {
+		t.Fatalf("want: 'proxy_pass http://right_proxy', got: '%s'", childP.GetValue())
+	}
+}
