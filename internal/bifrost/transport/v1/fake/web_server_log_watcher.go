10bda3e5dd10c9fc359d3edaa2b577ffd237b53e
ClessLi <316600949@qq.com>
2024-03-28 17:24:34 +0800

    chore: chage `Logger` package.

--- internal/bifrost/transport/v1/fake/web_server_log_watcher.go
+++ internal/bifrost/transport/v1/fake/web_server_log_watcher.go
@@ -2,13 +2,13 @@ package fake
 
 import (
 	pbv1 "github.com/ClessLi/bifrost/api/protobuf-spec/bifrostpb/v1"
-	log "github.com/ClessLi/bifrost/pkg/log/v1"
+	logV1 "github.com/ClessLi/component-base/pkg/log/v1"
 )
 
 type webServerLogWatcher struct{}
 
 func (w webServerLogWatcher) Watch(request *pbv1.LogWatchRequest, stream pbv1.WebServerLogWatcher_WatchServer) error {
-	log.Infof("watch web server log '%s'", request.ServerName)
+	logV1.Infof("watch web server log '%s'", request.ServerName)
 
 	return nil
 }
